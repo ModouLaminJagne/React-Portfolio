e9d1a13384fb78c1008a24370b343c2eb36494e9
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-11 20:16:24 +0000

    Updating the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
-
+import Header from './Components/Header'
 import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
