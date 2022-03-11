6902afc5f7cf812266912910c8eb38ea6eb9c930
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-11 18:50:11 +0000

    Building starts...

---
+++ src/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <h1>hello</h1>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+  
+  // function formatName(user) {
+  //   return user.firstName + ' ' + user.lastName;
+  // }
+  
+  // const user = {
+  //   firstName: 'Harper',
+  //   lastName: 'Perez',
+  // };
+  
+  
+  // function getGreeting() {
+  //   if (user) {
+  //     return <h1>Hello, {formatName(user)}!</h1>;
+  //   }
+  //   return <h1>Hello, Stranger.</h1>;
+  // }
+  // const element = <h1> {getGreeting()}</h1>;
+  
+  // ReactDOM.render(
+  //   // <grtGreeting />,
+  //   element,
+  //   document.getElementById('root')
+  // );
