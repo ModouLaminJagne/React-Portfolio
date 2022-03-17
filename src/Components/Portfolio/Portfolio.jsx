b97c51e276e544c67be2da313bcb9e0323894286
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-17 02:06:50 +0000

    Editing the Portfolio section.

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,17 +1,83 @@
 import React from 'react'
 import './Portfolio.css'
+import BC from '../../Assets/BC.webp'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className='container portfolio__container'>
-        
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={BC} alt="bc" />
+          </div>
+          <h3>this is a portfolio item</h3>
+          <a
+            href="https:/github.com"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https:/github.com"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Github
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={BC} alt="bc" />
+          </div>
+          <h3>this is a portfolio item</h3>
+          <a
+            href="https:/github.com"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https:/github.com"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Github
+          </a>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={BC} alt="bc" />
+          </div>
+          <h3>this is a portfolio item</h3>
+          <a
+            href="https:/github.com"
+            target="_blank"
+            rel="noreferrer"
+            className="btn"
+          >
+            Github
+          </a>
+          <a
+            href="https:/github.com"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-primary"
+          >
+            Github
+          </a>
+        </article>
       </div>
     </section>
-  )
+  );
 }
 
 export default Portfolio
