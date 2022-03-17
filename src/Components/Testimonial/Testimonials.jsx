89c480aca5d0621523074796c70b3c201c4e2ef9
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-17 04:05:48 +0000

    Editing the testimonials section.

--- src/Components/Testimonial/Testimonials.jsx
+++ src/Components/Testimonial/Testimonials.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import './Testimonials.css'
+import i16 from '../../Assets/i16.jpg';
+// import i16 from'../../Assets/i16.jpg';
+// import i16 from'../../Assets/i16.jpg';
+// import i16 from'../../Assets/i16.jpg';
+
 
 const Testimonials = () => {
   return (
-    <section>Testimonials</section>
-  )
+    <section id='testimonials'>
+      <h5>Received From Clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className='container testimonials__container'>
+        <article className='testimonial'>
+          <div className='client__avater'>
+            <img src={i16} alt='CA' />
+            <h5 className='client__name'>Someone</h5>
+            <small className='client__review'>
+              lorem ipsum blaa blaa blaa🤪
+            </small>
+          </div>
+        </article>
+        <article className='testimonial'>
+          <div className='client__avater'>
+            <img src={i16} alt='CA' />
+            <h5 className='client__name'>Someone</h5>
+            <small className='client__review'>
+              lorem ipsum blaa blaa blaa🤪
+            </small>
+          </div>
+        </article>
+        <article className='testimonial'>
+          <div className='client__avater'>
+            <img src={i16} alt='CA' />
+            <h5 className='client__name'>Someone</h5>
+            <small className='client__review'>
+              lorem ipsum blaa blaa blaa🤪
+            </small>
+          </div>
+        </article>
+        <article className='testimonial'>
+          <div className='client__avater'>
+            <img src={i16} alt='CA' />
+            <h5 className='client__name'>Someone</h5>
+            <small className='client__review'>
+              lorem ipsum blaa blaa blaa🤪
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default Testimonials
