5da8be55999e545499e88f414a92f1693bdc14b5
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-17 12:16:16 +0000

    Editing contact section.

--- src/Components/Contact/Contacts.jsx
+++ src/Components/Contact/Contacts.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import './Contacts.css'
+import {MdOutlineEmail} from 'react-icons/md'
 
 const Contacts = () => {
   return (
-    <section id='contact'>Contacts</section>
+    <section id='contact'>
+      <h5>Get in touch</h5>
+      <h2>Contact</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>my@email.com</h5>
+            <a href='mailto:myemail@gmail.com'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>my@email.com</h5>
+            <a href='mailto:myemail@gmail.com'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>my@email.com</h5>
+            <a href='mailto:myemail@gmail.com'>Send a message</a>
+          </article>
+        </div>
+        <form action=''></form>
+      </div>
+      </section>
   )
 }
 
