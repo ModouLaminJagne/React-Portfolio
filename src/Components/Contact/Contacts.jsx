afb3320ec3164073af885ea1257cd37bebe343af
ModouLaminJagne <89850614+ModouLaminJagne@users.noreply.github.com>
2022-03-17 12:48:13 +0000

    Contact section edited.

--- src/Components/Contact/Contacts.jsx
+++ src/Components/Contact/Contacts.jsx
@@ -1,37 +1,44 @@
 import React from 'react'
 import './Contacts.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { AiOutlineTwitter } from 'react-icons/ai';
 
 const Contacts = () => {
   return (
-    <section id='contact'>
-      <h5>Get in touch</h5>
+    <section id="contact">
+      <h5>Get In Touch</h5>
       <h2>Contact</h2>
-      <div className='container contact__container'>
-        <div className='contact__options'>
-          <article className='contact__option'>
-            <MdOutlineEmail/>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
             <h4>Email</h4>
             <h5>my@email.com</h5>
-            <a href='mailto:myemail@gmail.com'>Send a message</a>
+            <a href="mailto:myemail@gmail.com">Send a message</a>
           </article>
-          <article className='contact__option'>
-            <MdOutlineEmail/>
+          <article className="contact__option">
+            <AiOutlineLinkedin />
             <h4>Twitter</h4>
             <h5>my@twitter.com</h5>
-            <a href='mailto:myemail@gmail.com'>Send a message</a>
+            <a href="mailto:myemail@gmail.com">Send a message</a>
           </article>
-          <article className='contact__option'>
-            <MdOutlineEmail/>
+          <article className="contact__option">
+            <AiOutlineTwitter />
             <h4>Linkedin</h4>
             <h5>my@linkedin.com</h5>
-            <a href='mailto:myemail@gmail.com'>Send a message</a>
+            <a href="mailto:myemail@gmail.com">Send a message</a>
           </article>
         </div>
-        <form action=''></form>
+        <form action="">
+          <input type='text' name='name' placeholder='Your Name' required/>
+          <input type='text' name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required>Message</textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
-      </section>
-  )
+    </section>
+  );
 }
 
 export default Contacts
